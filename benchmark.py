fd848838f8af7bbb3ccef2758930a7d98b4843be
Maxim Kokryashkin <m.kokryashkin@tarantool.org>
2021-09-29 22:13:08 +0300

    added benchmark script

--- benchmark.py
+++ benchmark.py
@@ -1,13 +1,24 @@
-#!/bin/python
+#!/bin/python3
 import subprocess
 import sys
 
 from time import sleep
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 
 n_jobs = list(range(1, 9))
 n_samples = [1000, 10**6, 10**8]
-SLEEP_TIME = 4
+SLEEP_TIME = 10
 
 flags = {
         '--local':   ['mpiexec', '-np', 'jobs', './a.out', 'samples'],
@@ -34,11 +45,19 @@ for samples in n_samples:
         sleep(SLEEP_TIME)
         
         with open('output.txt', 'r') as f:
-            data = f.readlines[-2:]
-            stats[samples][job_count] = (float(data[0]), float(data[1]))
+            data = f.readlines()[-2:]
+            stats[samples][job_count] = float(data[0]) / float(data[1])
 
-for samples in n_samples:
-    print('SINGLE\tPARALLEL')
-    for job_count in n_jobs:
-        print(str(stats[samples][job_count][0]) + '\t' + str(stats[samples][job_count][1]))
+csv = ''
+for job_count in n_jobs:
+	csv += str(job_count) + ';'	
+	for samples in n_samples:
+		csv +=str(stats[samples][job_count]) + ';'
+	csv += '\n'
+with open('table.csv', 'w') as f:
+	f.write(csv)
+
+subprocess.run(['gnuplot', 'plot.sh'])
+print(bcolors.OKGREEN + 'Done!' + bcolors.ENDC)
+
 
