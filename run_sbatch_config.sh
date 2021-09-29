babe2fb521b20c04239a1e3f2d4403bc30dc664d
Maxim Kokryashkin <m.kokryashkin@tarantool.org>
2021-09-29 19:30:31 +0300

    added sbatch config

---
+++ run_sbatch_config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+#SBATCH --ntasks=8
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=RT
+#SBATCH --job-name="MIPT-PD homework"
+#SBATCH --comment="Run mpi from config"
+#SBATCH --output=output.txt
+#SBATCH --error=error.txt
+mpiexec ./a.out
